6e15ee77010eb4d53761201d38f6cfc9e6950ca0
artistatbl <jdaly2991@gmail.com>
2023-11-14 18:48:34 +0000

    vercel analytics package install

--- portfolio/src/components/Layout.js
+++ portfolio/src/components/Layout.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Analytics } from '@vercel/analytics/react';
+
 
 const Layout = ({children, className=""}) => {
 	return (
 		<div className={`w-full h-full inline-block z-0 bg-light p-32 ${className}`}>
 			{children}
+			<Analytics />
 		</div>
 	)
 }
