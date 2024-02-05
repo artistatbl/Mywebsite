703bceea73eb06a734158e78b6cc889e0073c599
Jean Daly <88166536+artistatbl@users.noreply.github.com>
2024-02-05 22:35:39 +0000

    adding site speed insights

--- portfolio/src/components/Layout.js
+++ portfolio/src/components/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Analytics } from '@vercel/analytics/react';
+import {SpeedInsights} from "@vercel/speed-insights/next";
 
 
 const Layout = ({children, className=""}) => {
@@ -8,6 +9,7 @@ const Layout = ({children, className=""}) => {
 		<div className={`w-full h-full inline-block z-0 bg-light p-32 dark:bg-dark xl:p-24 lg:p-16 md:p-12 sm:p8 ${className}`}>
 			{children}
 			<Analytics />
+			<SpeedInsights />
 		
 			
 		</div>
